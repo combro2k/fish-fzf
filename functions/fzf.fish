31f46b430404224ebcbf49a4a6f120217cb53ed9
Martijn van Maurik <git@vmaurik.nl>
2019-12-19 12:24:24 +0100

    Print to stderr

--- functions/fzf.fish
+++ functions/fzf.fish
@@ -1,6 +1,6 @@
 function fzf --description "A command-line fuzzy finder"
   if test ! -x $HOME/bin/fzf
-    printf 'Installing fzf into ~/bin/fzf...\n'
+    printf 'Installing fzf into ~/bin/fzf...\n' 1>&2
     curl --silent -fL https://github.com/junegunn/fzf-bin/releases/download/0.20.0/fzf-0.20.0-linux_amd64.tgz | tar zx -C ~/bin -- fzf
   end
 
