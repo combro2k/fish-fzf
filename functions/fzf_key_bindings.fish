dc2d6239a53f4b162e94c70784f4ba5a879616bb
Martijn van Maurik <git@vmaurik.nl>
2019-12-19 13:32:38 +0100

    Add history merge

--- functions/fzf_key_bindings.fish
+++ functions/fzf_key_bindings.fish
@@ -47,9 +47,11 @@ function fzf_key_bindings
       # history's -z flag was added in fish 2.4.0, so don't use it for versions
       # before 2.4.0.
       if [ "$FISH_MAJOR" -gt 2 -o \( "$FISH_MAJOR" -eq 2 -a "$FISH_MINOR" -ge 4 \) ];
+        history merge
         history -z | eval (__fzfcmd) --read0 --print0 -q '\(commandline\)' | read -lz result
         and commandline -- $result
       else
+        history merge
         history | eval (__fzfcmd) -q '\(commandline\)' | read -l result
         and commandline -- $result
       end
