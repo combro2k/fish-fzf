f66bc3bafa0091d82369cc98c8cf7f5b529356d6
Martijn van Maurik <git@vmaurik.nl>
2019-12-19 11:45:58 +0100

    Add keybindings

--- functions/fzf.fish
+++ functions/fzf.fish
@@ -1,5 +1,6 @@
 function fzf --description "A command-line fuzzy finder"
   if test ! -x $HOME/bin/fzf
+    printf 'Installing fzf into ~/bin/fzf...\n'
     curl --silent -fL https://github.com/junegunn/fzf-bin/releases/download/0.20.0/fzf-0.20.0-linux_amd64.tgz | tar zx -C ~/bin -- fzf
   end
 
