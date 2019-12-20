c0e990edfbe3e5469781de008eed73cb4dd85634
Martijn van Maurik <git@vmaurik.nl>
2019-12-20 09:46:52 +0100

    Update __fzf_complete based on jethrokuan's functions

--- functions/fzf_key_bindings.fish
+++ functions/fzf_key_bindings.fish
@@ -148,50 +148,131 @@ function fzf_key_bindings
     echo $dir
   end
 
-	function __fzf_complete -d 'fzf completion and print selection back to commandline'
-		# As of 2.6, fish's "complete" function does not understand
-		# subcommands. Instead, we use the same hack as __fish_complete_subcommand and
-		# extract the subcommand manually.
-		set -l cmd (commandline -co) (commandline -ct)
-		switch $cmd[1]
-			case env sudo
-				for i in (seq 2 (count $cmd))
-					switch $cmd[$i]
-						case '-*'
-						case '*=*'
-						case '*'
-							set cmd $cmd[$i..-1]
-							break
-					end
-				end
-		end
-		set cmd (string join -- ' ' $cmd)
+  function __fzf_complete -d 'fzf completion and print selection back to commandline'
+    set -l cmd (commandline -co) (commandline -ct)
+    switch $cmd[1]
+      case env sudo
+        for i in (seq 2 (count $cmd))
+          switch $cmd[$i]
+            case '-*'
+            case '*=*'
+            case '*'
+              set cmd $cmd[$i..-1]
+              break
+          end
+        end
+      end
+      set -l cmd_lastw $cmd[-1]
+      set cmd (string join -- ' ' $cmd)
+      set -l initial_query ''
+      test -n "$cmd_lastw"; and set initial_query --query="$cmd_lastw"
+      set -l complist (complete -C$cmd)
+      set -l result
+      test -z "$complist"; and return
+      set -l compwc (echo $complist | wc -w)
+      if test $compwc -eq 1
+        set result "$complist"
+      else
+        set -l query
+        string join -- \n $complist \
+          | eval (__fzfcmd) (string escape --no-quoted -- $initial_query) --print-query (__fzf_complete_opts) \
+          | cut -f1 \
+          | while read -l r
+        if test -z "$query"
+          set query $r
+        else
+          set result $result $r
+        end
+      end
+      if test -z "$query" ;and test -z "$result"
+        commandline -f repaint
+        return
+      end
+      if test -z "$result"
+        set result $query
+      end
+    end
+    set prefix (string sub -s 1 -l 1 -- (commandline -t))
+    for i in (seq (count $result))
+      set -l r $result[$i]
+      switch $prefix
+            case "'"
+              commandline -t -- (string escape -- $r)
+            case '"'
+              if string match '*"*' -- $r >/dev/null
+                commandline -t --  (string escape -- $r)
+              else
+                commandline -t -- '"'$r'"'
+              end
+            case '~'
+              commandline -t -- (string sub -s 2 (string escape -n -- $r))
+            case '*'
+              commandline -t -- $r
+        end
+        [ $i -lt (count $result) ]; and commandline -i ' '
+    end
+    commandline -f repaint
+  end
 
-		set -l complist (complete -C$cmd)
-		set -l result
-		string join -- \n $complist | sort | eval (__fzfcmd) -m --select-1 --exit-0 --header '\(commandline\)' | cut -f1 | while read -l r; set result $result $r; end
+  function __fzf_complete_opts_common
+    if set -q FZF_DEFAULT_OPTS
+      echo $FZF_DEFAULT_OPTS
+    end
+    echo --cycle --reverse --inline-info
+  end
 
-		set prefix (string sub -s 1 -l 1 -- (commandline -t))
-		for i in (seq (count $result))
-			set -l r $result[$i]
-			switch $prefix
-				case "'"
-					commandline -t -- (string escape -- $r)
-				case '"'
-					if string match '*"*' -- $r >/dev/null
-						commandline -t --  (string escape -- $r)
-					else
-						commandline -t -- '"'$r'"'
-					end
-				case '~'
-					commandline -t -- (string sub -s 2 (string escape -n -- $r))
-				case '*'
-					commandline -t -- (string escape -n -- $r)
-			end
-			[ $i -lt (count $result) ]; and commandline -i ' '
-		end
+  function __fzf_complete_opts_tab_accepts
+    echo --bind tab:accept,btab:cancel
+  end
 
-		commandline -f repaint
-	end
+  function __fzf_complete_opts_tab_walks
+    echo --bind tab:down,btab:up
+  end
 
+  function __fzf_complete_opts_preview
+    set -l file (status -f)
+    echo --with-nth=1 --preview-window=right:wrap --preview="fish\ '$file'\ __fzf_complete_preview\ '{1}'\ '{2..}'"
+  end
+
+  test "$argv[1]" = "__fzf_complete_preview"; and __fzf_complete_preview $argv[2..3]
+
+  function __fzf_complete_opts_0 -d 'basic single selection with tab accept'
+    __fzf_complete_opts_common
+    echo --no-multi
+    __fzf_complete_opts_tab_accepts
+  end
+
+  function __fzf_complete_opts_1 -d 'single selection with preview and tab accept'
+    __fzf_complete_opts_0
+    __fzf_complete_opts_preview
+  end
+
+  function __fzf_complete_opts_2 -d 'single selection with preview and tab walks'
+    __fzf_complete_opts_1
+    __fzf_complete_opts_tab_walks
+  end
+
+  function __fzf_complete_opts_3 -d 'multi selection with preview'
+    __fzf_complete_opts_common
+    echo --multi
+    __fzf_complete_opts_preview
+  end
+
+  function __fzf_complete_opts -d 'fzf options for fish tab completion'
+    switch $FZF_COMPLETE
+      case 0
+        __fzf_complete_opts_0
+      case 1
+        __fzf_complete_opts_1
+      case 2
+        __fzf_complete_opts_2
+      case 3
+        __fzf_complete_opts_3
+      case '*'
+        echo $FZF_COMPLETE
+    end
+    if set -q FZF_COMPLETE_OPTS
+      echo $FZF_COMPLETE_OPTS
+    end
+  end
 end
