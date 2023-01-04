012fdd4a5d25964231f7f7f055d8b2fae2c6c3a5
Martijn van Maurik <git@vmaurik.nl>
2023-01-04 15:34:47 +0100

    Update fzf

--- functions/fzf.fish
+++ functions/fzf.fish
@@ -1,7 +1,7 @@
 function fzf --description "A command-line fuzzy finder"
   if test ! -x $HOME/bin/fzf
     printf 'Installing fzf into ~/bin/fzf...\n' 1>&2
-    curl --silent -fL https://github.com/junegunn/fzf/releases/download/0.27.2/fzf-0.27.2-linux_amd64.tar.gz | tar zx -C ~/bin -- fzf
+    curl --silent -fL https://github.com/junegunn/fzf/releases/download/0.35.1/fzf-0.35.1-linux_amd64.tar.gz | tar zx -C ~/bin -- fzf
   end
 
   eval $HOME/bin/fzf "$argv"
